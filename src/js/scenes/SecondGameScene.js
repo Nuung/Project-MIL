5638cde653c9159d3c8e91c1ad612539e16588d9
Nuung <qlgks1@naver.com>
2019-05-10 13:39:50 +0200

    made the basic structure of ch2 game

--- src/js/scenes/SecondGameScene.js
+++ src/js/scenes/SecondGameScene.js
@@ -2,7 +2,7 @@ import BaseScene from "./BaseScene";
 
 // global game options
 var spawnAllowed = true; // check the value for spawn 
-var enemyGroup;
+var spawnTimer = 0;
 
 class SecondGameScene extends BaseScene {
 
@@ -22,13 +22,21 @@ class SecondGameScene extends BaseScene {
         this.cursors = this.input.keyboard.createCursorKeys();
 
         // enemy assets (Objects to Avoid)
-        this.hoverSprite = this.add.sprite(100, 100, 'cat'); // add test sprite
-        this.hoverSprite.setScale(2);
-        this.hoverSprite.setVisible(false);
-        enemyGroup = this.add.group(); // create group
-
-        // Phaser.Math.Between(0, this.game.config.width) 
-        // Phaser.Math.Between(0, this.game.config.height)
+        // group with all active platforms.
+        this.enemyGroup = this.add.group({
+            // once a platform is removed, it's added to the pool
+            removeCallback: function(platform){
+                platform.scene.enemyPool.add(platform);
+            }
+        });
+ 
+        // pool
+        this.enemyPool = this.add.group({
+            // once a platform is removed from the pool, it's added to the active platforms group
+            removeCallback: function(platform){
+                platform.scene.enemyGroup.add(platform);
+            }
+        });
         
         // animation (for test)
         this.anims.create({
@@ -39,6 +47,38 @@ class SecondGameScene extends BaseScene {
                 frames: [0,1,2,3]
             })
         })
+
+        // to limit player area
+        {
+            this.invisible_wallLeft = this.physics.add.sprite(100, 0, 'invisible_wall');
+            this.invisible_wallRight = this.physics.add.sprite(this.game.config.width - 100, 0, 'invisible_wall');
+            this.invisible_wallLeft.setDisplaySize(30, this.game.config.height * 2);
+            this.invisible_wallRight.setDisplaySize(30, this.game.config.height * 2);
+            this.invisible_wallLeft.fixedToCamera = true;
+            this.invisible_wallLeft.body.immovable = true;
+            this.invisible_wallLeft.body.allowGravity = false;
+            this.invisible_wallRight.fixedToCamera = true;
+            this.invisible_wallRight.body.immovable = true;
+            this.invisible_wallRight.body.allowGravity = false;
+        
+            this.physics.add.collider(this.player, this.invisible_wallLeft);
+            this.physics.add.collider(this.player, this.invisible_wallRight);
+        }
+    }
+
+    // the core of the script: platform are added from the pool or created on the fly
+    addPlatform(){
+
+        let platform;
+        
+        platform = this.physics.add.sprite(Phaser.Math.Between(0, this.game.config.width), 
+            Phaser.Math.Between(0, this.game.config.height), 
+            'cat'
+        );
+        
+        this.physics.moveTo(platform, this.player.x, this.player.y, Phaser.Math.Between(120, 240));
+        this.enemyGroup.add(platform);
+        platform.play("walk");
     }
 
     update(){
@@ -64,6 +104,28 @@ class SecondGameScene extends BaseScene {
             this.player.setVelocityY(0);
             // player.anims.play('turn');
         }
+
+        // recycling platforms
+        this.enemyGroup.getChildren().forEach(function(platform){
+            if(platform.x < - platform.displayWidth / 2 || platform.y < - platform.displayHeight / 2 ){
+                this.enemyGroup.killAndHide(platform);
+                this.enemyGroup.remove(platform);
+            } // if
+
+            if(this.physics.overlap(this.player, platform, null, null, this)){
+                this.enemyGroup.killAndHide(platform);
+                this.enemyGroup.remove(platform);
+            }
+        }, this);
+    
+        // adding new platforms
+        if(spawnTimer > 40 && spawnAllowed){
+            this.addPlatform();
+            spawnTimer = 0;
+        }
+
+        // spawn Time counter
+        spawnTimer++;
     }
 };
 

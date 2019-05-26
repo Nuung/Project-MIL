b88b1df46089dbb02816389640d2b33fa2b91f61
leahJU <wjddma1214@gmail.com>
2019-05-26 23:03:01 +0200

    add howtoscene and fix prescene

--- src/js/scenes/PreSecond.js
+++ src/js/scenes/PreSecond.js
@@ -1,5 +1,5 @@
 import BaseScene from "./BaseScene";
-
+var recieved;
 // playGame scene
 class PreSecond extends BaseScene {
     constructor(test) {
@@ -8,19 +8,57 @@ class PreSecond extends BaseScene {
         });
     }
 
+    init(data){
+        
+        console.log("im pre2"+data);
+        recieved = data;
+    }
+
     create(){
 
     // setting the back ground
-    this.background = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'secondBackground');
-    this.background.setDisplaySize(this.game.config.width, this.game.config.height);
-    this.add.image(0, 0, "second_intro_1").setOrigin(0).setDepth(0);
+    if(recieved == '1'){
+        this.add.image(0, 0, "firstBackground").setOrigin(0).setDepth(0);
+        this.add.image(0,0,"first_intro_2").setOrigin(0).setDepth(0);
+    }
+    else if(recieved == '2'){
+        this.background = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'secondBackground');
+        this.background.setDisplaySize(this.game.config.width, this.game.config.height);
+        this.add.image(0,0,"second_intro_2").setOrigin(0).setDepth(0);
+    }
 
-    let conti= this.add.image(400,430,"continue");
+   //start game button
+   let go = this.add.image(500,450,"go");
 
-        conti.setInteractive();
-        conti.on("pointerup",()=>{
-            this.scene.switch('PreSecond2');
-        })
+   go.setInteractive();
+   go.on("pointerup",()=>{
+       if(recieved == '1'){
+        console.log("I recieve"+ recieved);
+        this.scene.switch('FirstGameScene');
+       }
+       else if(recieved == '2'){
+        this.scene.switch('SecondGameScene');
+       }
+       this.scene.stop();
+   })
+
+//start howto scene
+    let howtobutton = this.add.image(300,450,"howtobutton");
+   
+    howtobutton.setInteractive();
+    howtobutton.on("pointerup",()=>{
+        if(recieved == '1'){
+        //    this.scene.pause();
+            this.scene.launch('HowtoScene',"1");
+        }
+        else if(recieved == '2'){
+        //    this.scene.pause();
+            this.scene.launch('HowtoScene',"2");
+        }
+        
+       this.scene.stop(); 
+       })
+       this.scene.bringToTop();
     }
     update(){
     

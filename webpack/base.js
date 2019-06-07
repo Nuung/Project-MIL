00554cd9f81d1781a4e9d29c86767e75bb539304
Nuung <qlgks1@naver.com>
2019-06-07 13:59:18 +0200

    init awsgit add --all

--- webpack/base.js
+++ webpack/base.js
@@ -6,6 +6,21 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
   mode: "development",
   devtool: "eval-source-map",
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    // useLocalIp: true,
+    // allowedHosts:[
+    //   '.ec2-3-16-158-49.us-east-2.compute.amazonaws.com',
+    //   'ec2-3-16-158-49.us-east-2.compute.amazonaws.com'
+    // ],
+    // public: 'ec2-3-16-158-49.us-east-2.compute.amazonaws.com:8080',
+    // host: '3.16.158.49',
+    // port: 8080,
+    // contentBase: path.resolve(__dirname, './src'), 
+    disableHostCheck: false, 
+    host: "0.0.0.0" // default : 127.0.0.1
+  },
   module: {
     rules: [
       {
